bdc10e19cd2898d79be84bf2effda86375b0ff82
kzjeef <kzjeef@3bafcfac-c55c-11de-bfbd-4d0f0f27293a>
2009-11-02 06:10:45 +0000

    reconstruct code, separete searcher and crawler.

--- test/search-test.py
+++ test/search-test.py
@@ -1,11 +1,12 @@
 import __init__
-from  searchengine import *
+from  searchengine import searcher
 
-s = searcher("searchindex.db")
+s = searcher.searcher("searchindex.db")
 
 query  = ["help","python","programming","ppp","pp","aaaa"]
 
 for q in query:
+    print "=" * 20
     print "~ * Start search '",q,"'"
     s.query(q)
     
